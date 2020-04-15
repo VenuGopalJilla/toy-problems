1fcde09fdcab39c1a6e5ba9be98b43290be7b4e5
Venu Gopal Jilla <venugopaljilla30@msitprogram.net>
2020-04-15 16:34:13 +0530

    Added Registration page to Flask app and also submitted form data to flask app

--- project1/application.py
+++ project1/application.py
@@ -4,7 +4,8 @@ from flask import Flask, session
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
-from flask import render_template
+from flask import render_template, request, session
+from flask_session import Session
 
 app = Flask(__name__)
 
@@ -13,9 +14,9 @@ app = Flask(__name__)
 #     raise RuntimeError("DATABASE_URL is not set")
 
 # # Configure session to use filesystem
-# app.config["SESSION_PERMANENT"] = False
-# app.config["SESSION_TYPE"] = "filesystem"
-# Session(app)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 # # Set up database
 # engine = create_engine(os.getenv("DATABASE_URL"))
@@ -25,3 +26,21 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/register", methods = ['GET','POST'])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    else :
+        session["data"] = []
+        name = request.form.get('username')
+        age = request.form.get('age')
+        dob = request.form.get('dob')
+        add = request.form.get('address')
+        session["data"].append(name)
+        session["data"].append(age)
+        session["data"].append(dob)
+        session["data"].append(add)
+        return render_template("register.html", notesdata = session["data"])
+
+
