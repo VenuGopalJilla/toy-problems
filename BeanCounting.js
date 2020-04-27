fe647e3d5842a1660e642cc9e5337cc1f820d2eb
Venu Gopal Jilla <venugopaljilla30@msitprogram.net>
2020-04-27 17:54:45 +0530

    First Problem completed.

---
+++ BeanCounting.js
@@ -0,0 +1,30 @@
+
+
+function countBs(str) {
+    var count = 0
+    for (let i = 0; i < str.length; i++) {
+        const letter = str[i]
+        if (str[i] === "B") {
+            count++
+        }
+    }
+    return count
+}
+
+function countChar(str, str1) {
+    var count1 = 0
+    for(let j = 0; j < str.length; j++){
+        const letter1 = str[j]
+        if (str[j] === str1) {
+            count1++
+        }
+    }
+    return count1
+}
+
+var strin = "BBBBBB"
+const num = countBs(strin)
+const num1 = countChar(strin, "B")
+
+console.log("Count of B's in " + strin + " is : " + num)
+console.log("Count of B's in " + strin + " is : " + num)
